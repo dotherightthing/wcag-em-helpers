637590530cbfb0b48663e7229529d2c0b9899066
Dan Smith <dev@dotherightthing.co.nz>
2020-08-14 00:37:25 +1200

    Housekeeping

--- background.js
+++ background.js
@@ -1,8 +1,7 @@
-chrome.runtime.onMessage.addListener(
-    function(message, callback) {
-        if (message === 'runContentScript') {
-            chrome.tabs.executeScript({
-                file: 'contentScript.js'
-            });
+chrome.runtime.onMessage.addListener((message, callback) => {
+    if (message === 'runContentScript') {
+        chrome.tabs.executeScript({
+            file: 'contentScript.js'
+        });
     }
 });
